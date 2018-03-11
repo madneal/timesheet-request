3ef1072d4b07285f3207e70d4be3c2dccb9790bf
neal1991 <neal1991@sina.com>
2018-03-11 15:58:09 +0800

    add stationinfo and format timesheet.json t obtain timesheet1.json :art:

--- crawler.py
+++ crawler.py
@@ -2,16 +2,39 @@ import requests
 import json
 
 base_url = 'http://m.shmetro.com/interface/metromap/metromap.aspx?func=fltime&line='
-station_info_url = 'http://m.shmetro.com/interface/metromap/metromap.aspx?func=stationInfo&stat_id=0111'
+station_info_url = 'http://m.shmetro.com/interface/metromap/metromap.aspx?func=stationInfo&stat_id='
 station_timesheet_url = 'http://m.shmetro.com/interface/metromap/metromap.aspx?func=fltime&stat_id=111'
 
 line_num_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17]
-result = []
 filename = 'timesheet.json'
 station_file = 'station.json'
 
 
+def read_json(filename):
+    with open(filename, 'r', encoding='utf8') as f:
+        return json.load(f);
+
+
+def write_json(filename, data):
+    with open(filename, 'w', encoding='utf8') as f:
+        json.dump(data, f, ensure_ascii=False)
+        f.close()
+
+
+def carwler_station_info():
+    keys = read_json('key.json')
+    result = []
+    for key in keys:
+        station_name = list(key.keys())[0]
+        station_id = key[station_name]
+        url = station_info_url + station_id
+        station_info = make_request(url)
+        result = result + station_info
+    write_json('stationInfo.json', result)
+
+
 def crawler():
+    result = []
     for line_num in line_num_arr:
         print('request for line ' + str(line_num))
         make_request(base_url + str(line_num))
@@ -22,10 +45,27 @@ def crawler():
 
 def make_request(url):
     r = requests.get(url)
-    result_arr = r.json()
-    result.append(result_arr)
+    return r.json()
 
+
+def format_station_info():
+    timesheet = read_json('timesheet1.json')
+    data = []
+    result = {}
+    for arr in timesheet:
+        data = data + arr
+    print(data)
+    for ele in data:
+        name = ele['name']
+        if not name in result.keys():
+            result[name] = [ele]
+        else:
+            result[name].append(ele)
+    print(result)
+    print(timesheet)
 
 
 if __name__ == '__main__':
-    crawler()
+    # crawler()
+    # carwler_station_info()
+    format_station_info()
